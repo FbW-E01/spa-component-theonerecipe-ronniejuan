9ea48d4876351eb8a243fc0b45f34732654ef4e5
ronniejuan <hofmannronnie@gmail.com>
2021-07-09 14:31:14 +0200

    theonce recipe with style. y

---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+
+import './Footer.css';
+
+function Footer() {
+    return (
+    <div className="footer">
+       <h4>Características</h4>
+       <div className="footerRecipe">
+         <ul>
+           <li>Sabor: <span>Citrico, fresco</span></li>
+           <li>Tipo: <span>Cocktail</span></li>
+           <li>Dificultad: <span>Media</span></li>
+         </ul>
+       </div>
+    </div>
+    )
+}
+
+export default Footer;
---
+++ src/Heading.jsx
@@ -0,0 +1,20 @@
+// export default function Title (){
+
+// return <p>Im just a Title</p>
+// }
+
+
+
+import './Heading.css';
+
+function Heading() {
+    return (
+    <div className="Heading">
+       <h1>PISCO SOUR</h1>
+       <hr/>
+       <h3>TRADICION PERUANA</h3>
+    </div>
+    )
+}
+
+export default Heading;
